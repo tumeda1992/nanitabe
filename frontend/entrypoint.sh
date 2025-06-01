3f382d82859ae90b2ef12cc8fab81fad9a6dbc63
setsumaru1992 <setsumaru1992@gmail.com>
2025-06-01 16:34:34 +0900

    ローカルPCでNODE_ENV=productionでdocker composeできるよう修正

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-yarn install
 port=18100
 
-if [ $NODE_ENV = "production" ]; then
-   yarn build
-   yarn start -p ${port}
+if [ "$NODE_ENV" = "production" ]; then
+  # なぜかテストのコードもビルドしようとするので、一旦テスト用ライブラリをいれる
+  yarn install --production=false
+  yarn build
+  yarn start -p ${port}
 
    # tail -n 1 -f package.json > /dev/null # デバッグ
 else
-   yarn dev -p ${port}
+  yarn install
+  yarn dev -p ${port}
 fi
