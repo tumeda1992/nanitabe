abd06b2bdcd9bd6428cfce83c78466b940c4a940
setsumaru1992 <setsumaru1992@gmail.com>
2025-09-21 14:56:21 +0900

    tagの集約ルートを作成

---
+++ backend/app/domain/business/food/dish/tag/root.rb
@@ -0,0 +1,17 @@
+module Business::Food::Dish::Tag
+  class Root < ::Business::Base::Entity
+    attribute :id, :integer
+
+    attribute :user_id, :integer
+    validates :user_id, presence: true
+
+    attribute :content, :any
+    validates :content, presence: true
+
+    def set_id(new_id)
+      raise "新規作成時以外idを変更できません" if self.id.present?
+
+      self.id = new_id
+    end
+  end
+end
