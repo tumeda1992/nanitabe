0932104e1788f27d96edee76a07cc30b7654c100
setsumaru1992 <setsumaru1992@gmail.com>
2025-09-23 22:37:59 +0900

    Mutations::MealのUpdateのmutationにgraphqlに新モジュールを適用

--- backend/app/graphql/mutations/meal/update_meal_with_new_dish.rb
+++ backend/app/graphql/mutations/meal/update_meal_with_new_dish.rb
@@ -11,13 +11,24 @@ module Mutations::Meal
 
     def resolve(meal:, dish:, dish_source:, dish_source_relation_detail: nil, dish_tags: nil)
       ActiveRecord::Base.transaction do
-        updated_meal, created_dish = ::Business::Dish::Command::Meal::UpdateMealWithNewDishCommand.call(
+        dish_source_relation = if dish_source_relation_detail.present? && dish_source.present?
+                                 ::Business::Food::Dish::Usecase::Params::DishSourceRelation.build_relation(
+                                   dish_source.type,
+                                   dish_source.id,
+                                   dish_source_relation_detail.detail_value_of(dish_source.type)
+                                 )
+                               end
+
+        created_dish = ::Business::Food::Dish::Usecase::AddCommand.call(
           user_id: context[:current_user_id],
-          dish_for_create: dish.convert_to_command_param,
-          dish_source_for_read: dish_source.convert_to_command_param,
-          dish_source_relation_detail: dish_source_relation_detail&.convert_to_command_param(dish_source&.type),
-          dish_tags: (dish_tags || [])&.map {|dish_tag| dish_tag.convert_to_command_param},
-          meal_for_update: meal.convert_to_command_param,
+          dish_params: dish.convert_to_command_param(use_food_module: true),
+          dish_source_relation:,
+          dish_tags: (dish_tags || [])&.map {|dish_tag| dish_tag.convert_to_command_param(use_food_module: true) },
+        )
+
+        updated_meal = ::Business::Food::Meal::Usecase::UpdateCommand.call(
+          user_id: context[:current_user_id],
+          meal_params: meal.convert_to_command_param(use_food_module: true, dish_id: created_dish.id)
         )
         {
           meal_id: updated_meal.id,
--- backend/app/graphql/types/input/meal/meal_for_update.rb
+++ backend/app/graphql/types/input/meal/meal_for_update.rb
@@ -6,5 +6,13 @@ module Types::Input::Meal
     argument :date, GraphQL::Types::ISO8601Date, required: false
     argument :meal_type, Int, required: false
     argument :comment, String, required: false
+
+    def convert_to_command_param(use_food_module: false, dish_id: nil)
+      if use_food_module
+        ::Business::Food::Meal::Usecase::Params::Meal.new(**to_hash, dish_id:)
+      else
+        super()
+      end
+    end
   end
 end
