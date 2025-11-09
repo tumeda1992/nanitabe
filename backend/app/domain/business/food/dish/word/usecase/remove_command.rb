cd55888340d543589566cd3728b3cf81dcb7cdb2
setsumaru1992 <setsumaru1992@gmail.com>
2025-11-09 21:08:53 +0900

    Business::Food::Dish::Word::Usecase::UpdateCommand 作成

--- backend/app/domain/business/food/dish/word/usecase/remove_command.rb
+++ backend/app/domain/business/food/dish/word/usecase/remove_command.rb
@@ -1,6 +1,7 @@
 module Business::Food::Dish::Word
   class Usecase::RemoveCommand < ::Business::Base::Command
     attribute :normalize_word_id, :integer
+    validates :normalize_word_id, presence: true
 
     def call
       NormalizeWord.find(normalize_word_id).destroy!
---
+++ backend/app/domain/business/food/dish/word/usecase/update_command.rb
@@ -0,0 +1,38 @@
+module Business::Food::Dish::Word
+  class Usecase::UpdateCommand < ::Business::Base::Command
+    attribute :normalize_word_id, :integer
+    validates :normalize_word_id, presence: true
+
+    attribute :source, :string
+    validates :source, presence: true
+
+    attribute :destination, :string
+    validates :destination, presence: false
+
+    def call
+      existing_normalize_word = NormalizeWord.find(normalize_word_id)
+
+      normalized_source = Usecase::NormalizeCommand.call(
+        string_sequence: source,
+        use_db_normalize_word: false
+      )
+      normalized_destination = if destination.present?
+                                 Usecase::NormalizeCommand.call(
+                                   string_sequence: destination,
+                                   use_db_normalize_word: false
+                                 )
+                               else
+                                 normalized_source
+                               end
+
+      existing_normalize_word.update!(
+        entered_source: source,
+        entered_destination: destination.presence || "",
+        source: normalized_source,
+        destination: normalized_destination,
+      )
+
+      Usecase::ReflectLatestNormalizeWordCommand.call
+    end
+  end
+end
