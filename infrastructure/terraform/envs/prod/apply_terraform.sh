08b1479357c94dc65f04cce6011da3678b7eeafd
tumeda1992 <44469281+tumeda1992@users.noreply.github.com>
2025-11-24 20:02:44 +0900

    フロントエンドについて、イメージ最新化の自動化 (#136)
    * buildspec.ymlをgit管理にする
    
    * DockerfileでAWSコマンドを使うための環境変数をbuildspecにセット
    
    * Dockerfileもcommit
    
    * CodePipeline経由のCDをテスト用の環境変数で実行
    
    * CodePipeline経由のCDを本番用の環境変数で実行するコード作成

--- infrastructure/terraform/envs/prod/apply_terraform.sh
+++ infrastructure/terraform/envs/prod/apply_terraform.sh
@@ -3,7 +3,17 @@ export $(grep -v '^#' /etc/opt/app_setting_files/nanitabe/.env | xargs)
 
 export TF_VAR_bucket_name=${TERRAFROM_STATE_S3_BUCKET}
 export TF_VAR_dynamodb_table_name=${TERRAFROM_STATE_DYNAMODB_TABLE}
+
+export TF_VAR_aws_account_id=${AWS_ACCOUNT_ID}
+export TF_VAR_aws_access_key=${AWS_ACCESS_KEY_ID}
+export TF_VAR_aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+
+export TF_VAR_backend_host=${BACKEND_PROD_HOST}
+
 export TF_VAR_route53_zone_id=${ROUTE53_HOSTZONE_ID}
 export TF_VAR_route53_name=${ROUTE53_HOSTZONE_NAME}
-export TF_VAR_backend_host=${BACKEND_PROD_HOST}
+
+export TF_VAR_codebuild_artifact_s3_bucket=${CODEBUILD_ARTICACT_S3_BUCKET}
+export TF_VAR_aws_code_connection_id_to_github=${AWS_CODE_CONNECTION_ID_TO_GITHUB}
+
 terraform apply
