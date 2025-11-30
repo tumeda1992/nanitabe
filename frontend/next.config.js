ef38de8ae32e688e31deaaba60141122e18d4331
setsumaru1992 <setsumaru1992@gmail.com>
2025-11-30 14:40:44 +0900

    APIGatewayで動くNext.jsが参照する静的コンテンツを環境変数で指定できるようにする(buildspec.yml, next.config.js)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,30 +9,26 @@ const nextConfig = (() => {
   const baseConfig = {
     compress: false, // API Gateway で gzip させるため
     // 環境変数で出し分けられるようにする（サーバでクライアントでも読めるように接頭辞をつける）
-    assetPrefix: isProd ? 'https://d2ewo1yy2ahstj.cloudfront.net' : undefined, // prod用
+    // assetPrefix: isProd ? 'https://d2ewo1yy2ahstj.cloudfront.net' : undefined, // prod用
     // assetPrefix: isProd ? 'https://d1qmtpt8svn0j2.cloudfront.net' : undefined, //verify-infra用
+    assetPrefix: isProd ? process.env.NEXT_PUBLIC_ASSET_ORIGIN : undefined,
   };
 
-  // 本番以外は特に無視しなくていいならここで返してOK
   if (!isProd) return baseConfig;
 
-  // 本番だけ test/spec 系を ignore-loader で無視
   return {
     ...baseConfig,
     turbopack: {
       rules: {
-        // x.spec.ts / x.spec.tsx / x.test.ts / x.test.tsx など
         '**/*.spec.*': {
           loaders: ['ignore-loader'],
         },
         '**/*.test.*': {
           loaders: ['ignore-loader'],
         },
-        // /something/specHelper/... 配下
         '*/specHelper/**': {
           loaders: ['ignore-loader'],
         },
-        // renderWithApollo.tsx 単独
         '**/renderWithApollo.tsx': {
           loaders: ['ignore-loader'],
         },
