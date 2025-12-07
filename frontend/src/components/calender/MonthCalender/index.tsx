5e3ba13eb835d269fba7a4e854c0e86bad965455
setsumaru1992 <setsumaru1992@gmail.com>
2025-12-07 10:44:55 +0900

    monthカレンダーに別途dateを使うところで無限再レンダーが起こっていたので対処

--- frontend/src/components/calender/MonthCalender/index.tsx
+++ frontend/src/components/calender/MonthCalender/index.tsx
@@ -69,7 +69,8 @@ export default (props: Props) => {
           })?.meals || [];
 
         return {
-          date: addHours(date, 9), // なぜか9時間ずらさないとRailsに前日で送られるので暫定的に回避させる
+          // なぜか9時間ずらさないとRailsに前日で送られるので暫定的に回避させる
+          date: new Date(`${format(date, 'yyyy-MM-dd')}T09:00:00`),
           dayLabel: weekdays[getDay(date)],
           meals,
         };
